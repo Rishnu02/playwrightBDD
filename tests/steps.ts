2ae48e72f6dc65220712f252435be0df1a82f0b2
Rishnu <rishnuk25@gmail.com>
2026-04-20 23:29:11 +0530

    tested

--- tests/steps.ts
+++ tests/steps.ts
@@ -4,3 +4,7 @@ export {test, expect} from '@playwright/test'
 Given('User is login successfully', async({page}){
    console.log("tested"); 
 })
+
+When('testing git command', async({page}){
+    console.log("testing")
+})
